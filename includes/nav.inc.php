337c8a91408d6235d93e9b04c42ace1ab10f775c
Emmanuel <s3784431@stdeunt.rmit.edu.au>
2020-10-27 21:16:28 +1100

    initial commit

---
+++ includes/nav.inc.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-primary mb-3">
+    <div class="container">
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <p><span class="logo1">ABC GYM</span></p>
+            <div class="navbar-nav">
+                <p><span class="logo1">ABC GYM</span></p>
+            </div>
+            <div class="navbar-nav ml-auto">
+                <a href="index.php" class="nav-item nav-link">Home</a>
+                <a href="aboutus.php" class="nav-item nav-link">About Us</a>
+                <a href="gallery.php" class="nav-item nav-link">Gallery</a>
+                <a href="contacts.php" class="nav-item nav-link">Contacts</a>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php require_once('includes/head.inc.php'); ?>
+  </head>
+  <body>
+    
+  </body>
+</html>

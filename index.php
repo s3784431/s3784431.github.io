74f1cf74328a2c8b0081571497b7cab9fdf32a0b
Emmanuel <s3784431@stdeunt.rmit.edu.au>
2020-10-27 21:24:23 +1100

    changed the code structure

--- index.php
+++ index.php
@@ -1,10 +1,42 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <?php require_once('includes/head.inc.php'); ?>
-  </head>
-  <body>
-    <?php require_once('includes/nav.inc.php'); ?>
-    
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name ="author" content ="Emmanuel Zaitu">
+    <title>GMF</title>
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+   
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-3">
+            <div class="container">
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <p><span class="logo1">ABC GYM</span></p>
+                    <div class="navbar-nav">
+                        <p><span class="logo1">ABC GYM</span></p>
+                    </div>
+                    <div class="navbar-nav ml-auto">
+                        <a href="index.php" class="nav-item nav-link">Home</a>
+                        <a href="aboutus.php" class="nav-item nav-link">About Us</a>
+                        <a href="gallery.php" class="nav-item nav-link">Gallery</a>
+                        <a href="contacts.php" class="nav-item nav-link">Contacts</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
   </body>
 </html>
